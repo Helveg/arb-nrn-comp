60c152dc291bb8af406f1808eec6ed4f7c7fe792
Robin De Schepper <robin.deschepper93@gmail.com>
2022-02-22 12:17:40 +0100

    nrn?

--- plots/single_cell_val_nrn.py
+++ plots/single_cell_val_nrn.py
@@ -7,15 +7,19 @@ import dbbs_models
 import dbbs_models.test
 import time
 import pickle
-from arbor import single_cell_model
+from patch import p
 
 def plot():
     nrn_data = {}
+    pkl_data = {}
     for name, model in vars(dbbs_models).items():
         if name.endswith("Cell"):
             print("Running", name)
             nrn_data[name] = []
             cell = model()
-            t = time.time()
+            time = p.time
             dbbs_models.test.quick_test(cell, duration=1000)
-            nrn_data[name] = list(cell.Vm)
+            nrn_data[name] = (list(time), list(cell.Vm))
+            arb_data[name] = go.Figure(go.Scatter(x=nrn_data[name][0], y=nrn_data[name][1]))
+    with open("arb_sc.pkl", "wb") as f:
+        pickle.dump(pkl_data, f)
