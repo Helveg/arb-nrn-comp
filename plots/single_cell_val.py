4e01809e3d4dc4d1357c770239e04a02b0fe89a2
Robin De Schepper <robin.deschepper93@gmail.com>
2022-02-22 12:09:55 +0100

    short

--- plots/single_cell_val.py
+++ plots/single_cell_val.py
@@ -12,14 +12,14 @@ def plot():
     arb_data = {}
     for name, model in vars(dbbs_models).items():
         if name.endswith("Cell"):
-            print("Running", name)
+            print("Running", name, flush=True)
             arb_model = single_cell_model(model.cable_cell(labels=arbor.label_dict({"midpoint": "(root)"})))
             arb_model.properties.set_ion(
                 ion="h", valence=1, int_con=1.0, ext_con=1.0, rev_pot=-34
             )
             arb_model.properties.catalogue = model.get_catalogue()
             arb_model.probe('voltage', '"midpoint"', frequency=10)
-            arb_data[name] = []
             cell = model()
-            arb_model.run(1000, dt=0.025)
-            arb_data[name].append()
+            arb_model.run(10, dt=0.025)
+            arb_data[name] = go.Figure(go.Scatter(x=m.traces[0].time, y=m.traces[0].value))
+    return arb_data
